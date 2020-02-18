05dd2c1aa7c69de03e1284888f0f923a0414fc6c
S.W.A.T. Robotics <frc1806@gmail.com>
2020-02-17 21:01:33 -0600

    Timed Mobility Auto

---
+++ src/main/java/com/team1806/frc2020/auto/modes/TimedMobilityAuto.java
@@ -0,0 +1,12 @@
+package com.team1806.frc2020.auto.modes;
+
+import com.team1806.frc2020.auto.AutoModeEndedException;
+import com.team1806.frc2020.auto.AutoModeExecutor;
+import com.team1806.frc2020.auto.actions.TimedMobilityAction;
+
+public class TimedMobilityAuto extends AutoModeBase {
+    @Override
+    protected void routine() throws AutoModeEndedException {
+        runAction(new TimedMobilityAction(.75));
+    }
+}
