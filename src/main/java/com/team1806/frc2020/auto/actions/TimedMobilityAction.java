e4de7acc164c104714978b85f058ff305ad0f107
S.W.A.T. Robotics <frc1806@gmail.com>
2020-02-17 20:09:16 -0600

    Straight on Launch and Mobility Auto

---
+++ src/main/java/com/team1806/frc2020/auto/actions/TimedMobilityAction.java
@@ -0,0 +1,37 @@
+package com.team1806.frc2020.auto.actions;
+
+import com.team1806.frc2020.subsystems.Drive;
+import com.team1806.lib.util.DriveSignal;
+import edu.wpi.first.wpilibj.Timer;
+
+public class TimedMobilityAction implements Action {
+    double startTime;
+    double runTime;
+    Drive driveTrain = Drive.getInstance();
+
+    public TimedMobilityAction(double _runTime) {
+        startTime = Timer.getFPGATimestamp();
+        runTime = _runTime;
+    }
+
+    @Override
+    public void start() {
+        startTime = Timer.getFPGATimestamp();
+        driveTrain.setHighGear(false);
+    }
+
+    @Override
+    public void update() {
+        driveTrain.setOpenLoop(new DriveSignal(.8,.8));
+    }
+
+    @Override
+    public boolean isFinished() {
+        return Timer.getFPGATimestamp() - startTime >= runTime;
+    }
+
+    @Override
+    public void done() {
+        driveTrain.stop();
+    }
+}
---
+++ src/main/java/com/team1806/frc2020/auto/modes/StraightOnLaunchAndMobilityAuto.java
@@ -0,0 +1,16 @@
+package com.team1806.frc2020.auto.modes;
+
+import com.team1806.frc2020.auto.AutoModeEndedException;
+import com.team1806.frc2020.auto.actions.LaunchAction;
+import com.team1806.frc2020.auto.actions.TimedMobilityAction;
+import com.team1806.frc2020.game.Shot;
+
+public class StraightOnLaunchAndMobilityAuto extends AutoModeBase{
+
+
+    protected void routine() throws AutoModeEndedException {
+        runAction(new LaunchAction(Shot.STRAIGHT_ON_AUTOLINE,10));
+        runAction(new TimedMobilityAction(.2));
+    }
+
+}
